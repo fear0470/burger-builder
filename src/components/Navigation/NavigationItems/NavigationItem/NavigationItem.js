c435591001fdd3a8c7365102117943f8ee6daaa8
Nicholas Fearns <you@example.com>
2019-11-10 22:13:19 +0000

    imported the style files and called the classes in the respective components

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import classes from './NavigationItem.css';
 
-const navigationItem = (props) => ();
+const navigationItem = (props) => (
+    <li className={classes.NavigationItem}><a href="/">A Link</a>
+    </li>
+);
 
 export default navigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import classes from './NavigationItems.css';
 
 const navigationItems = () => (
-    <ul>
-        <li><a href="/">A Link</a>
-        </li>
+    <ul className={classes.NavigationItems}> 
+        
     </ul>
 );
 
