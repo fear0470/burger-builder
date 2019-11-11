d27961411c17512f6d212a682ccaed2331e5569a
Nicholas Fearns <you@example.com>
2019-11-11 21:33:48 +0000

    fixed the styling and classname to fix problem with displaying the logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,10 +4,10 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolbar = (props) => (
+const toolbar = ( props ) => (
     <header className={classes.Toolbar}>
         <DrawerToggle clicked={props.drawerToggleClicked} />
-        <div>
+        <div className={classes.Logo}>
             <Logo />
         </div>
         <nav className={classes.DesktopOnly}>
