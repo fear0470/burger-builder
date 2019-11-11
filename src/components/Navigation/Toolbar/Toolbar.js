361add22a4c33fc9742542e6ef4218be16cd8b19
Nicholas Fearns <you@example.com>
2019-11-11 01:30:44 +0000

    working on the backdrop

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -9,7 +9,7 @@ const toolbar = (props) => (
         <div>
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
