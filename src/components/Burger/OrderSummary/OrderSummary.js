6da2660418bf1408a831c3ad0813de586c7a27f6
Nicholas Fearns <you@example.com>
2019-11-11 23:41:29 +0000

    preparing for merge

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,34 +3,33 @@ import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-    componentWillUpdate() {
-        console.log('[OrderSummary] WillUpdate');
+    componentDidUpdate() {
+        console.log('[OrderSummary] DidUpdate');
     }
 
     render () {
-        const ingredientSummary = Object.keys(this.props.ingredients)
+        const ingredientSummary = Object.keys( this.props.ingredients )
             .map( igKey => {
                 return (
                     <li key={igKey}>
-                        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.renderprops.ingredients[igKey]}
+                        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
                     </li> );
             } );
 
-
-    return (
+        return (
             <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients: </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {Number.parseFloat( this.props.price ).toFixed( 2 )}</strong></p>
-            <p>Continue to Checkout</p>
-            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed( 2 )}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
             </Aux>
         );
     }
-} 
+}
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -8,8 +8,8 @@ class Modal extends Component {
         return nextProps.show !== this.props.show;
     }
 
-    componentWillUpdate () {
-        console.log('[Modal] WillUpdate');
+    componentDiDUpdate () {
+        console.log('[Modal] DiDUpdate');
     }
 
     render () {
