9406d72be61bf8a173648fc0f5ea3b14382da492
Nicholas Fearns <you@example.com>
2019-11-15 02:06:26 +0000

    turned withErrorHandler into a classful component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <Aux>
-                <Modal>
-                    Something went wrong.
-                </Modal>
-                <WrappedComponent {...props} />
-            </Aux>
-        );
-    }
+const withErrorHandler =  (WrappedComponent, axios ) => {
+    return class extends Component {
+        render () {
+            return (
+                <Aux>
+                    <Modal>
+                        Something went wrong.
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
+    } 
 }
 
 export default withErrorHandler;
