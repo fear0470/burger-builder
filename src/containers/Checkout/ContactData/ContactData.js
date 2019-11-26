0e82d347a849b4e66bdac27d86c8f3be9a2a7fd5
Nicholas Fearns <you@example.com>
2019-11-26 00:25:17 +0000

    created initial form to gather contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react'; 
 
 class ContactData extends Component {
-    
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h4>Enter your Contact Data</h4>
+                <form>
+                    <input type="text" name="" placeholder="Your Name" />
+                    <input type="email" name="email" placeholder="Your Email" />
+                    <input type="text" name="street" placeholder="Street" />
+                    <input type="text" name="postal" placeholder="Postal Code" />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default ContactData;
