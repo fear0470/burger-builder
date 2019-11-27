d63bdc3971bde6d8861be860710bf2637f4fe236
Nicholas Fearns <you@example.com>
2019-11-27 03:10:37 +0000

    getting ready to show indivual orders on the orders.js page

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Order from '../../components/Order/Order.js';
 
 class Orders extends Component {
     render () {
-        return ();
+        return (
+            <div>
+                <Order />
+            </div>
+        );
     }
 }
 
