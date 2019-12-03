c42160416b63852c70aa74ba6c72276283d9f1f1
Nicholas Fearns <you@example.com>
2019-12-03 20:06:19 +0000

    outputting order info

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,7 +4,7 @@ import classes from './Order.css';
 const order = (props) => (
     <div className={classes.Order}>
         <p>Ingredients: </p>
-        <p>Price: <strong>CAD $7.50</strong></p>
+        <p>Price: <strong>CAD ${Number.parseFloat(props.price).toFixed(2)} </strong></p>
     </div>
 )
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -31,7 +31,9 @@ class Orders extends Component {
             <div>
                 {this.state.orders.map(order => (
                     <Order 
-                        key={order.id} />
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
                 ))}
             </div>
         );
