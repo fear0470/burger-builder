22068afc927cd4f78e492e491eca4152d0c9cf5e
Nicholas Fearns <you@example.com>
2019-12-03 20:15:51 +0000

    outputting the ingredients in the Order comp.

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: </p>
-        <p>Price: <strong>CAD ${Number.parseFloat(props.price).toFixed(2)} </strong></p>
-    </div>
-)
+const order = (props) => {
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredientName,
+                amount: props.ingredients[ingredientName]
+            }
+        );
+    }
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: </p>
+            <p>Price: <strong>CAD ${Number.parseFloat(props.price).toFixed(2)} </strong></p>
+        </div>
+    );
+    
+}
 
 export default order;
