4e5422a3af61f8cc4c9480d6a39f4bca052dc3a7
Nicholas Fearns <you@example.com>
2019-12-03 23:16:36 +0000

    reviewed the order and contact data pages to fix problem when pushing data to the firebase db

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,12 +15,13 @@ class ContactData extends Component {
         loading: false
     }
 
-    orderHandler = (event) => {
-      this.setState({ loading: true });
+    orderHandler = ( event ) => {
+        event.preventDefault();
+        this.setState( { loading: true } );
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-           customer: {
+            customer: {
                 name: 'Nick Fearns',
                 address: {
                     street: '555 Test Lane',
@@ -31,27 +32,27 @@ class ContactData extends Component {
             },
             deliveryMethod: 'fastest'
         }
-        axios.post('/orders.json', order)
+        axios.post( '/orders.json', order )
             .then( response => {
-                this.setState({ loading: false });
+                this.setState( { loading: false } );
                 this.props.history.push('/');
             } )
-            .catch(error => {
-                this.setState({ loading: false });
+            .catch( error => {
+                this.setState( { loading: false } );
             } );
     }
 
     render () {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="" placeholder="Your Name" />
-                <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
                 <input className={classes.Input} type="text" name="street" placeholder="Street" />
                 <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
-        if (this.state.loading) {
+        if ( this.state.loading ) {
             form = <Spinner />;
         }
         return (
