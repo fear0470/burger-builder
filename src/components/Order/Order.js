a35d0a1c21092b748b6080e73738aa576c1ffd5b
Nicholas Fearns <you@example.com>
2019-12-03 20:48:53 +0000

    styled the ingredients as they are displayed

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -13,9 +13,24 @@ const order = (props) => {
         );
     }
 
+    const ingredientOutput = ingredients.map(ig => {
+        return <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}
+            key={ig.name}>
+            {ig.name}
+            ({ig.amount})
+        </span>;
+    })
+
     return (
         <div className={classes.Order}>
-            <p>Ingredients: </p>
+            <p>Ingredients: {ingredientOutput} </p>
             <p>Price: <strong>CAD ${Number.parseFloat(props.price).toFixed(2)} </strong></p>
         </div>
     );
