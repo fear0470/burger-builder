0bfae42dd5983088b02e6f08c931776ee5c49b37
Nicholas Fearns <you@example.com>
2019-12-05 00:26:53 +0000

    added some styling for the Input.js comp and imported the classes

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import classes from './Input.css';
 
 const input = (props) => {
     let inputElement = null;
 
     switch ( props.inputType ) {
         case ( 'input' );
-            inputElement = <input {...props} />;
+            inputElement = <input className={} {...props} />;
             break;
         case ( 'textarea' );
-            inputElement = <textarea {...props} />;
+            inputElement = <textarea className={} {...props} />;
             break;
         default: 
-            inputElement = <input {...props} />;
+            inputElement = <input className={} {...props} />;
     }
 
     return (
