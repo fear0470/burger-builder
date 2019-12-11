a45bad10079544fa16a56f7755d387de894bff23
Nicholas Fearns <you@example.com>
2019-12-11 01:47:14 +0000

    getting ready for push

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -91,9 +91,10 @@ class ContactData extends Component {
             <form>
                 {formElementsArray.map(formElement => (
                     <Input
+                        key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.elementConfig} />
+                        value={formElement.config.value} />
                 ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
