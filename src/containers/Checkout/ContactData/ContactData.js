4202b9fca42647500c5f303d35c4b04a71bbd2e8
Nicholas Fearns <you@example.com>
2020-01-16 22:59:35 +0000

    missing comma

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -79,7 +79,7 @@ class ContactData extends Component {
                 elementType: 'select',
                 elementConfig: {
                     options: [
-                        { value: 'fastest', displayValue: 'Fastest'}
+                        { value: 'fastest', displayValue: 'Fastest'},
                         { value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
