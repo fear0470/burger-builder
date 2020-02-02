00181234704c0d92a376444722c2be55277b99fd
Nicholas Fearns <you@example.com>
2020-02-02 23:54:55 +0000

    hooking up the ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,7 +16,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
-        
+        this.props.onInitIngredients();
     }
 
     updatePurchaseState (ingredients) {
@@ -50,7 +50,7 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
         let orderSummary = null;
-        let burger = this.state.error ? <p>Ingredients can not be loaded.</p> : <Spinner />;
+        let burger = this.props.error ? <p>Ingredients can not be loaded.</p> : <Spinner />;
 
         if (this.props.ings) {
             burger = (
@@ -86,7 +86,8 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        error: state.error
     };
 }
 
@@ -94,6 +95,7 @@ const mapDispatchToProps = dispatch => {
     return {
         onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
         onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
 
     };
 }
