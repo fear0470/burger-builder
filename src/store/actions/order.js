4bb27c9684a6e8cb6056f0cfa1b857787ab15d47
Nicholas Fearns <you@example.com>
2020-03-02 00:49:35 +0000

    working on order.js in the actions folder

---
+++ src/store/actions/order.js
@@ -0,0 +1,30 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        error: error
+    };
+}
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState( { loading: false } );
+                this.props.history.push('/');
+            } )
+            .catch( error => {
+                this.setState( { loading: false } );
+            } );
+    };
+};
