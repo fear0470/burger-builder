38a7a92b022515fc366c3aeec93147dc8b66f171
Nicholas Fearns <you@example.com>
2020-03-05 02:30:27 +0000

    making sure I update the other files that had the name changed

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -17,11 +17,14 @@ export const purchaseBurgerFail = (error) => {
 }
 
 export const purchaseBurgerStart = () => {
-
+    return {
+        type: actionTypes.PURCHASE_BURGER_START
+    };
 };
 
 export const purchaseBurger = (orderData) => {
     return dispatch => {
+        dispatch(purchaseBurgerStart());
         axios.post( '/orders.json', orderData )
             .then( response => {
                 dispatch( purchaseBurgerSuccess( response.data, orderData ) );
